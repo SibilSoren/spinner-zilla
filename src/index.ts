6144d88503b28793e964563317b970961d2ed63a
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2024-11-17 21:00:09 +0530

    Added all spinners

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 import './styles.css';
 
 export * from './components/CircleSpinner';
+export * from './components/ClockSpinner';
+export * from './components/DNASpinner';
+export * from './components/DotSpinner';
+export * from './components/FlipSpinner';
+export * from './components/GalaxySpinner';
+export * from './components/HeartbeatSpinner';
+export * from './components/MatrixSpinner';
 export * from './components/PulseSpinner';
+export * from './components/SquareSpinner';
+export * from './components/StarSpinner';
+export * from './components/WaveSpinner';
